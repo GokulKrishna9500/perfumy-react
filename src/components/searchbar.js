06f39341ac61b250d34423dac2ca90d27320aaaf
GokulKrishna9500 <gokulkrishna.sp@gmail.com>
2024-06-03 14:12:15 +0530

    feat: Perfumy app updated

---
+++ src/components/searchbar.js
@@ -0,0 +1,10 @@
+//searchbar
+function Searchbar() {
+    return (
+      <div class="searchbar">
+        <input type="text" placeholder="Search"></input>
+      </div>
+    )
+  }
+
+  export default Searchbar
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App';
+
+import './style.css';
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
+  <App></App>
+)
